03f3e44e203d013466ba181677a50b3f23291761
Jag Zhang <zzzsgsb@gmail.com>
2022-05-14 21:22:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-
-apt update
-apt install -y python3-pip ffmpeg zip unzip libsm6 libxext6 libgl1-mesa-dev libosmesa6-dev libgl1-mesa-glx patchelf
-
+sudo apt install -y python3-pip ffmpeg zip unzip libsm6 libxext6 libgl1-mesa-dev libosmesa6-dev libgl1-mesa-glx patchelf
 
 # install Python dependencies in a virtualenv
-virtualenv -p python3 venv
-. venv/bin/activate
 pip3 install numpy scipy gym dotmap matplotlib tqdm opencv-python tensorboardX moviepy plotly gdown
 pip3 install torch==1.4.0
 pip3 install torchvision==0.5.0
