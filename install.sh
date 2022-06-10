23272351b637f97b7527ad6ccba200e1ad36bceb
Jag Zhang <jajajag@users.noreply.github.com>
2022-06-10 13:59:22 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,5 @@ sudo apt install -y python3-pip ffmpeg zip unzip libsm6 libxext6 libgl1-mesa-dev
 pip3 install numpy scipy gym dotmap matplotlib tqdm opencv-python tensorboardX moviepy plotly gdown
 pip3 install torch==1.4.0
 pip3 install torchvision==0.5.0
-pip3 install mujoco_py==1.50.1.68
+#pip3 install mujoco_py==1.50.1.68
+pip3 install mujoco_py
