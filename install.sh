7a5d866aec7afece4ded3c89b2705f1c527c5db3
Jag Zhang <jajajag@users.noreply.github.com>
2022-06-10 01:50:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 sudo apt install -y python3-pip ffmpeg zip unzip libsm6 libxext6 libgl1-mesa-dev libosmesa6-dev libgl1-mesa-glx patchelf
 
+# create conda environment for recovery-rl
+conda create --name recovery-rl
+conda activate recovery-rl
+
 # install Python dependencies in a virtualenv
 pip3 install numpy scipy gym dotmap matplotlib tqdm opencv-python tensorboardX moviepy plotly gdown
 pip3 install torch==1.4.0
 pip3 install torchvision==0.5.0
 pip3 install mujoco_py==1.50.1.68
-
-# download demonstration files for shelf, maze
-gdown --id 10M7pzsvKP4DcDDqxctDzx_opGfoV6rVt --output recovery_rl_data.zip
-unzip recovery_rl_data.zip
-rm recovery_rl_data.zip
